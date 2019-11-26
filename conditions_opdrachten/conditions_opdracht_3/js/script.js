0e16b6894fee8a623bcb6fc6099df9f11bc656da
youp bos <youpbos121@gmail.com>
2019-11-26 12:38:16 +0100

    opdracht 3 af

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,22 @@
 //javascript code
+
+function checkSaldo() {
+    
+    var salaris = document.getElementById("salaris").value ;
+
+    var bankSaldo = 0;
+
+    var restTotaal = 0 + salaris;
+
+    if(restTotaal < 500 ){
+        alert("tewijnig salaris");
+    }
+
+    if(restTotaal > 500){
+        alert("salaris plus bonus")
+    }
+    
+    if(restTotaal == 500){
+        alert("salaris")
+    }
+}
