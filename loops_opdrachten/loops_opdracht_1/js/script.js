5eb4e50f82a9e6b2935f89ea6a4607f7175c7848
youp bos <youpbos121@gmail.com>
2019-11-26 13:34:40 +0100

    opdracht 1 loops af

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+
+function geefGetallen(){
+    var ingevultdgetal = document.getElementById("startgetal").value;
+
+    for(var teller = 1; teller <= 100; teller += 1){
+
+        alert(teller * ingevultdgetal);
+    }
+}
