5c03831c7baefbe5f09ac3b9e97f3fc1444b1940
youp bos <youpbos121@gmail.com>
2019-11-26 11:10:56 +0100

    opdracht 1, 2 af

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+
+function checkSaldo() {
+    
+    var geldOpnamen = document.getElementById("geldOpnamen").value ;
+
+    var bankSaldo = 1000;
+
+    var restTotaal = 1000 - geldOpnamen;
+
+    if(restTotaal < 25 ){
+        alert("sorry, je hebt te weinig saldo!");
+    }
+}
