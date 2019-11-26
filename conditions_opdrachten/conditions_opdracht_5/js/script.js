df20e0ef0fb0bfe4825fab70a662e65d7864258d
youp bos <youpbos121@gmail.com>
2019-11-26 13:21:42 +0100

    opdracht 5 af

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+
+var myOperator = "+";
+var myNummber1 = prompt ("geef aub een getal op?");
+var myNummber2 = prompt ("geef aub een getal op?");
+var resultaat;
+
+if( myOperator == "+") {
+    resultaat = Number(myNummber1) + Number(myNummber2);
+    document.getElementById("resultaat").innerHTML = resultaat;
+
+}else if(myOperator == "-") {
+    resultaat = myNummber1 - myNummber2;
+    document.getElementById("resultaat").innerHTML = resultaat;
+}
