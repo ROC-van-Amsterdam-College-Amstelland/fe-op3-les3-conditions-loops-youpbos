cf9404b1997ce80adf6d7e94d7bed8b02943cc92
youp bos <youp.bos@student.rocva.nl>
2019-12-05 14:52:50 +0100

    opdracht 5.2

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -4,6 +4,13 @@ var nummer = 1;
 
 
 for (var teller = 1; teller <= 20; teller++) {
-    console.log(teller);
-    document.getElementById("resultaat").innerHTML += teller;
+
+    if (teller % 2 == 0) {
+        document.getElementById("resultaat").innerHTML += "<h6>" + teller + "</h6>";
+    }
+
+    else {
+        console.log(teller);
+    }
+
 }
