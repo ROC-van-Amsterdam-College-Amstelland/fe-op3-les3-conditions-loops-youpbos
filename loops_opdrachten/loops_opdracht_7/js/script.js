fd4749c0a64c093baf04ce039dfba8c00d5747bc
youp bos <youp.bos@student.rocva.nl>
2019-12-05 15:06:56 +0100

    opdracht 7 helemaal af

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -6,6 +6,10 @@ for (var teller = 1; teller <= 100; teller++) {
         document.getElementById("resultaat").innerHTML += "<strong>VET</strong><br>";
     }
 
+    if (teller % 5 == 0) {
+        document.getElementById("resultaat").innerHTML += "<strong>COOL</strong><br>";
+    }
+
     else {
         document.getElementById("resultaat").innerHTML += teller + "<br>";
     }
