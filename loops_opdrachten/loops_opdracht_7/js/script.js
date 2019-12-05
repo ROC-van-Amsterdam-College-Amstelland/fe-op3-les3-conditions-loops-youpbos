7c532b6cef6d8657da86848fd57d3b7565dff7cb
youp bos <youp.bos@student.rocva.nl>
2019-12-05 15:04:56 +0100

    opdracht 7 af

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+
+for (var teller = 1; teller <= 100; teller++) {
+
+    if (teller % 3 == 0) {
+        document.getElementById("resultaat").innerHTML += "<strong>VET</strong><br>";
+    }
+
+    else {
+        document.getElementById("resultaat").innerHTML += teller + "<br>";
+    }
+
+}
