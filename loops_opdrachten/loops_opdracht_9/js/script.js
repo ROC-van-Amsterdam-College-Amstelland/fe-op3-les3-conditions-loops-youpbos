ce7ad2bd8f32578c5576d1e37649c16c6020d07a
youp bos <youp.bos@student.rocva.nl>
2019-12-11 10:55:35 +0100

    opdracht 9 af

--- loops_opdrachten/loops_opdracht_9/js/script.js
+++ loops_opdrachten/loops_opdracht_9/js/script.js
@@ -1,1 +1,18 @@
 //javascript code
+
+for (teller = 1; teller <= 5; teller++) {
+    console.log(teller);
+
+    if (teller == 5) {
+        alert("het einde is bereikt")
+    }
+
+}
+
+for (teller = 5; teller >= 0; teller--) {
+    console.log(teller);
+
+    if (teller == 0) {
+        alert("het begin is het begin")
+    }
+}
