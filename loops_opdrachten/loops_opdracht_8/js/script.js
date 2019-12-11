2bf5aa5b33f88f23688a05ecfeb39f18a4e496c5
youp bos <youp.bos@student.rocva.nl>
2019-12-11 10:43:12 +0100

    opdracht 8 af

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -1,1 +1,9 @@
 //javascript code
+
+for (teller = 1; teller <= 50; teller++) {
+    document.getElementById("resultaat").innerHTML += teller + "<br>";
+
+    if (teller == 25) {
+        alert("Het getal 25 is net geen bullseye!")
+    }
+}
